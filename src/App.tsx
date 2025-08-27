ee532b8615b66da56331f5f1f10dbd172fbaa072
Jéssica Motta <jessicalimamotta@gmail.com>
2025-08-27 16:49:05 -0300

    add api

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,68 @@
 import { useEffect, useState } from "react";
-import initialTasks from "./db_tasks.json"
 import type { Task } from "./types";
 import './App.css'
+import axios from "axios";
+import { v4 as uuidv4 } from 'uuid'
 
 function App() {
-
 
   const [tasks, setTasks] = useState<Task[]>([])
   const [newTaskText, setNewTaskText] = useState("");
 
-
   useEffect(() => {
-    setTasks(initialTasks)
+    axios.get("http://localhost:3001/tasks").then(response => {
+      setTasks(response.data)
+    })
   }, [])
 
-  const handleAddTask = (e: React.FormEvent) => {
+  // add new task
+  const handleAddTask = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    const newTask: Task = {
-      id: Date.now(),
-      text: newTaskText,
-      isCompleted: false
+    // check if input isn't empty
+    if (newTaskText !== '') {
+      const newTask: Task = {
+        id: uuidv4(),
+        text: newTaskText,
+        isCompleted: false
+      }
+
+      const response = await axios.post("http://localhost:3001/tasks", newTask);
+      setTasks([...tasks, response.data]);
+      setNewTaskText("");
     }
-
-    setTasks([...tasks, newTask]);
-    setNewTaskText("");
+    else {
+      window.alert("Precisa digitar o nome da tarefa")
+    }
   }
 
-  const handleToggleComplete = (id: number) => {
-    const updatedTasks = tasks.map(task =>
-      task.id === id ? {...task, isCompleted: !task.isCompleted} : task
-    )
-    setTasks(updatedTasks);
+  // toggle task
+  const handleToggleComplete = (id: string) => {
+    const updatedTasks = tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, isCompleted: !task.isCompleted }
+      }
+      return task;
+    })
+
+    const taskToUpdate = updatedTasks.find(task => task.id === id)
+    if (taskToUpdate) {
+      axios.put(`http://localhost:3001/tasks/${id}`, taskToUpdate).catch(error => console.log('Error updating item', error))
+    }
+    setTasks(updatedTasks)
   }
 
-  const handleRemoveTask = (id: number) => {
-       const uptadedTasks = tasks.filter(task => task.id !== id);
-       setTasks(uptadedTasks)  
+
+  // remove task
+  const handleRemoveTask = async (id: string) => {
+    try {
+      await axios.delete(`http://localhost:3001/tasks/${id}`)
+      const updatedTasks = tasks.filter(task => task.id !== id)
+      setTasks(updatedTasks)
+
+    } catch (error) {
+      console.log('Error deleting time', error)
+    }
   }
 
   return (
@@ -50,16 +76,13 @@ function App() {
         {tasks.map(task => (
           <li key={task.id} className={task.isCompleted ? 'completed' : ''}>
             <span onClick={() => handleToggleComplete(task.id)}>
-              <input type="checkbox" checked={task.isCompleted} onChange={() => handleToggleComplete(task.id)}/>
+              <input type="checkbox" checked={task.isCompleted} onChange={() => handleToggleComplete(task.id)} />
               {task.text}
             </span>
-              <button onClick={() => handleRemoveTask(task.id)}>Del</button>
+            <button onClick={() => handleRemoveTask(task.id)}>Del</button>
           </li>
         ))}
-
       </ul>
-
-
     </>
   )
 }
